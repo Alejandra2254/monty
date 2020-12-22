33a887749d289cc44474d4803dd820029cb7d597
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-12-21 19:43:41 -0500

    fixed lines

--- j.m
+++ j.m
@@ -1,4 +1,7 @@
-push 1
-push 2
+         pall
+                                     pall
+
+
 push 3
 pall
+,
--- main.c
+++ main.c
@@ -48,6 +48,7 @@ int main(int argc, char **argv)
 	{
 		if (*buff == '\n')
 		{
+			con++;
 			continue;
 		}
 		line = strtok(buff, " \t\n");
